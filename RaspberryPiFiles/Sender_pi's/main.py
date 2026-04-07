c55e80a4e2b1f3253841703c6ce997024babe908
JacobLByers <jaleby03@gmail.com>
2026-04-07 13:45:22 -0500

    updated sending pi's code

--- RaspberryPiFiles/Sender_pi's/main.py
+++ RaspberryPiFiles/Sender_pi's/main.py
@@ -1,36 +1,45 @@
-
 # Code from https://timhanewich.medium.com/how-to-use-a-reyax-rylr998-lora-module-with-a-raspberry-pi-pico-and-other-microcontrollers-4ae52686836f
 # imports
-# import reyax
+import reyax
 import time
-# import machine
-from pythonping import ping
+import machine
+import network
+# from pythonping import ping
+
+
+def Scan_Wifi_and_Send():
+    networks = wlan.scan()
+
+    entries = []
+    for net in networks:
+        ssid = net[0].decode("utf-8").repace(",","").replace("|","")
+        rssi = net[3]
+        entries.append(f"{ssid}:{rssi}")
+
+
+    payload = "|".join(entries)
+
+
+    # LoRa only allows 240 bytes of data
+    if len(payload) > 200:
+        payload = payload[:200]
+
+    lora.send(0, payload.encode("ascii"))
+
+wlan = network.Wlan()
+wlan.active(True)
+
 
 
 # setting up GPIO pins and lora for the radio transmitter
-# u = machine.UART(0, baudrate=115200, tx=machine.Pin(0), rx=machine.Pin(1))
-# lora = reyax.RYLR998(u)
+u = machine.UART(0, baudrate=115200, tx=machine.Pin(0), rx=machine.Pin(1))
+lora = reyax.RYLR998(u)
 
-
-# if lora.pulse: # the pulse function returns True if a simple test command was able to validate the existence of the connected RYLR998 module.
-    # print("Connected")
-
+if lora.pulse: # the pulse function returns True if a simple test command was able to validate the existence of the connected RYLR998 module.
+    print("Connected")
 
 while True:
-
     for i in range(60): # send a message every 15 minutes
-        # ping wifi
-        result = ping("google.com", count=20, timeout=2, interval = 5)
-        if result.success():
-            print(result.rtt_avg_ms)
-
-
-        # add wifi to text note
-
         # sleep for 15 seconds
-        # time.sleep(15)
-
-
-    # lora.send(0, "Hello World".encode("ascii"))
-    print("test")
-        
+        Scan_Wifi_and_Send()
+        time.sleep(15)
